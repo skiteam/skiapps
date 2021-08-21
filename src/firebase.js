2ef321e99f4f44c349789c2338fdcd2cf22a54f4
NaokiIshii <naoki.harry.ishii727@gmail.com>
2021-08-21 21:49:15 +0900

    firebaseの設定

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDppEeExWNKucC9DtG10Qk6IFbVnyoNbqE",
+  authDomain: "skiapps-7bbd6.firebaseapp.com",
+  projectId: "skiapps-7bbd6",
+  storageBucket: "skiapps-7bbd6.appspot.com",
+  messagingSenderId: "330906452849",
+  appId: "1:330906452849:web:81845c4e31708cc867acd0",
+  measurementId: "G-QS8F2FKTYF",
+}
+
+firebase.initializeApp(firebaseConfig)
