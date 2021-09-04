3f9a648628be8b62fd595357259608acc7b75417
Abraso <kachi_pea.61@docomo.ne.jpl>
2021-09-05 00:05:44 +0900

    ぴえん

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,26 +1,30 @@
 <template>
  <div class="container">
     <div class="buttons">
-        <button class="alcohol">お酒</button>
-        <button class="appetizer">おつまみ</button>
-        <button class="both">どっちも</button>
+        <button v-on:click="makeTag" class="alcohol">ビール</button>
+        <button v-on:click="makeTag1" class="appetizer">おつまみ</button>
+        <button v-on:click="makeTag2" class="both">どっちも</button>
     </div>
+<select v-model="selected">
+  <option disabled value="">Please select one</option>
+  <option>#ビール</option>
+  <option>#おつまみ</option>
+  <option>#どっちも</option>
+</select>
+<p>
+<span>{{ selected }}</span>
     <div class="format">
-        <textarea type="text" rows="5" maxlength="1000" v-model="postContents" />
+        <input type="text" rows="1" maxlength="5" v-model="tagvalue" placeholder="↑で選択" />
+        <br>    
+        <textarea type="text" rows="5" maxlength="1000" v-model="postContents" placeholder="書いてみよう！" />
         <p>
-        <input type="file"  ref="preview" v-on:change="selectImage" name="file" accept="image/jpeg, image/png" multiple>
-
+        <input type="file"  ref="preview" v-on:change="selectImage" name="file" accept="image/jpeg, image/png" multiple />
         <div class="picture" v-if="selectedImageUrl">
+            <!-- <img v-for="selectedImageUrl in files" :key="selectedImageUrl"> -->
             <img :src="selectedImageUrl" alt="選択された画像" class="image">
         </div>
         <div> 
             <input type="submit" v-on:click="post" class="posting" value="投稿"> 
-            
-            <!-- <textarea v-model="text"/>
-            <button v-on:click="postTweet">ツイート</button>
-             <p v-for="item in tweets" :key="item.id">
-              {{item.text}}
-            </p> -->
         </div>   
     </div>
  </div>
@@ -33,18 +37,23 @@ import firebase from "firebase";
 export default {
     data() {
     return {
+      selected:"",
+      tagvalue:"",
       postContents: "",
       selectedImageUrl: "",
+      images:[],
       tweets: [],
     }
   },
   methods: {
       post(){
           const item = {
+              selected:this.selected,
               postContents:this.postContents,
               selectedImageUrl:this.selectedImageUrl,
+              photos:this.images,
           };
-              firebase.firestore().collection("tweets")
+        firebase.firestore().collection("tweets")
         .add(item)
         .then(ref => {
           this.tweets.push({
@@ -53,24 +62,26 @@ export default {
           });
         });
           },
-      
-    //   postTweet() {
-    // const item = {
-    //     text:this.text 
-    //   };
-    //     firebase.firestore().collection("tweets")
-    //     .add(item)
-    //     .then(ref => {
-    //       this.tweets.push({
-    //         id: ref.id,
-    //         ...item
-    //       });
-    //     });
-    //   },
       selectImage:function(){
-         console.log(this.$refs.preview.files[0])
-         const file = this.$refs.preview.files[0];
+         const file = this.$refs.preview.files[0]
          this.selectedImageUrl = URL.createObjectURL(file)
+         const Image=URL.createObjectURL(file)
+         this.images.push(Image)
+         },
+      makeTag:function(){
+          this.tagvalue=""
+          const tag="#ビール"
+          this.tagvalue=tag
+      },
+      makeTag1:function(){
+          this.tagvalue=""
+          const tag="#おつまみ"
+          this.tagvalue=tag
+      },
+      makeTag2:function(){
+          this.tagvalue=""
+          const tag="#どっちも"
+          this.tagvalue=tag
       },
    created: function(){
       firebase
@@ -94,5 +105,6 @@ export default {
 </script>
 
 <style>
+
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 import Home from "../views/Home.vue"
 import About from "../views/About.vue"
-import post from "../views/post.vue"
+import post from "../views/Posting.vue"
 
 Vue.use(VueRouter)
 
